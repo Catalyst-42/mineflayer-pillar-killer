6b1787693f63b8aadc60dd85a345acff27f4813a
Catalyst-42 <72890979+Catalyst-42@users.noreply.github.com>
2022-08-01 17:05:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (process.argv.length < 4 || process.argv.length > 6) {
 const bot = mineflayer.createBot({
   host: process.argv[2],
   port: parseInt(process.argv[3]),
-  username: process.argv[4] ? process.argv[4] : 'gps',
+  username: process.argv[4] ? process.argv[4] : 'demolisher',
   password: process.argv[5]
 })
 
